14fe629534b78f261966b41501159a5b41847122
kyni0124@naver.com <kyni0124@naver.com>
2025-01-02 00:10:10 +0900

    callAccept commit

--- front/src/page/CallAccept.jsx
+++ front/src/page/CallAccept.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import Button from '../components/button'
+import phone from "../../public/icon/Phone.svg";
+import speaker from "../../public/icon/Speaker.svg";
+
+import "../css/callAccept.scss";
 
 function CallAccept() {
+
+  const btnData = [
+    {
+      text: "68저 9856",
+      link: "",
+      disabled: true
+    },
+    {
+      text: "3분 뒤 도착 예정",
+      link: "",
+      disabled: true
+    }
+  ]
+
   return (
-    <div>
-      CallAccept
+    <div className="call-accept">
+      <div className="buttons">
+        {btnData.map((btn, index) => (
+          <Button key={index} btnData={btn} />
+        ))}
+      </div>
+      <div className="icons">
+        <button className="icon-button">
+          <img src={speaker} alt="speaker" />
+        </button>
+        <button className="icon-button">
+          <img src={phone} alt="phone" />
+        </button>
+      </div>
     </div>
+    
   )
 }
 
