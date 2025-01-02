d9c97ec37ba8c905e9f9601818e657c92da7667b
kimDongIll <ehddlf2893@naver.com>
2025-01-02 17:08:38 +0900

    fixed css

--- front/src/page/GetOnGetOff.jsx
+++ front/src/page/GetOnGetOff.jsx
@@ -1,31 +1,30 @@
-import React, {useState} from 'react'
-import Map from '../components/Map'
-import Button from '../components/button'
-import SearchBar from '../components/SearchBar'
-import '../css/getOnGetOff.scss'
+import React, { useState } from "react";
+import Map from "../components/Map";
+import Button from "../components/button";
+import SearchBar from "../components/SearchBar";
+import "../css/getOnGetOff.scss";
 
 function GetOnGetOff() {
-
-  const [selectedAddress, setSelectedAddress] = useState('');
+  const [selectedAddress, setSelectedAddress] = useState("");
   const height = 600;
   const btnData = {
     text: "이 위치로 설정",
-    link: "/"
-  }
+    link: "/",
+  };
 
   const handleSearch = (address) => {
     setSelectedAddress(address);
-    console.log('검색한 주소:', address);
+    console.log("검색한 주소:", address);
   };
 
   return (
     <div className="get-on-get-off">
       <SearchBar onSearch={handleSearch} />
-        <Map height={height}/>
+      <Map height={height} />
       <div className="button-container">
-        <Button btnData={btnData}/>
+        <Button btnData={btnData} />
       </div>
     </div>
-  )
+  );
 }
-export default GetOnGetOff
+export default GetOnGetOff;
--- front/src/page/VoiceRecordList.jsx
+++ front/src/page/VoiceRecordList.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/voiceRecordList.scss'
+import SearchBar from '../components/SearchBar'
 
 function VoiceRecordList() {
+  const [selectedAddress, setSelectedAddress] = useState('');
 
   const result = [
     {text: '가산디지털단지역 7호선'},
@@ -10,14 +12,22 @@ function VoiceRecordList() {
     {text: '가산디지털단지역환승 사거리'},
     {text: '올리브영 가산 디지털단지역'},
     {text: '리안헤어 가산디지털단지역'},
+    {text: '리안헤어 가산디지털단지역'},
+    {text: '리안헤어 가산디지털단지역'},
+    {text: '리안헤어 가산디지털단지역'},
   ]
+
+  const handleSearch = (address) => {
+    setSelectedAddress(address);
+    console.log('검색한 주소:', address);
+  };
 
   return (
     <div className='voiceRecordList'>
-      <div>input</div>
+      <SearchBar onSearch={handleSearch}/>
       <div className='voiceRecordListContainer'>
         {result.map((item,idx)=>{
-          return (<div className='voiceRecordListText'>{item.text}</div>)
+          return (<div key={idx} className='voiceRecordListText'>{item.text}</div>)
         })}
       </div>
     </div>
