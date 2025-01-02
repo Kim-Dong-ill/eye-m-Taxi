7680ec540b9c0e6da0d142355441142800976c01
kimDongIll <ehddlf2893@naver.com>
2025-01-02 16:05:43 +0900

    mainpage voiceList

--- front/src/page/VoiceRecordList.jsx
+++ front/src/page/VoiceRecordList.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import '../css/voiceRecordList.scss'
 
 function VoiceRecordList() {
+
+  const result = [
+    {text: '가산디지털단지역 7호선'},
+    {text: '가산디지털단지역환승 노상공영주차장'},
+    {text: '가산디지털단지역환승 사거리'},
+    {text: '가산디지털단지역환승 사거리'},
+    {text: '올리브영 가산 디지털단지역'},
+    {text: '리안헤어 가산디지털단지역'},
+  ]
+
   return (
-    <div>
-      VoiceRecordList
+    <div className='voiceRecordList'>
+      <div>input</div>
+      <div className='voiceRecordListContainer'>
+        {result.map((item,idx)=>{
+          return (<div className='voiceRecordListText'>{item.text}</div>)
+        })}
+      </div>
     </div>
   )
 }
