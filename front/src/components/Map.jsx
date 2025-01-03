bcd6a26a5f0723112b1f6d6f0be8323ab70d94cc
kyni0124@naver.com <kyni0124@naver.com>
2025-01-03 10:48:36 +0900

    gps commit

--- front/src/components/Map.jsx
+++ front/src/components/Map.jsx
@@ -1,15 +1,25 @@
 import React, { useRef } from 'react'
 import useMap from '../hooks/useMap'
 import { mapConfig } from '../config/mapConfig'
+import '../css/map.css'
 
-function Map({height}) {
+function Map({height, currentPosition}) {
 
     const mapContainerRef = useRef(null);
+    const currentMapCenter = useMap(mapContainerRef, mapConfig.defaultStyle, mapConfig, currentPosition);
+    
+    console.log('현재 지도 중심 좌표:', currentMapCenter);
 
-    useMap(mapContainerRef, mapConfig.defaultStyle, mapConfig);
   return (
-    <div ref={mapContainerRef} className='mapContainer' style={{width: '100%', height: `${height}px`}}/>
-  )
+    <div className="map-wrapper" style={{ position: 'relative', width: '100%', height: `${height}px` }}>
+            <div ref={mapContainerRef} className='mapContainer' style={{width: '100%', height: '100%'}}/>
+            <img 
+                src="/icon/marker2.svg"  
+                alt="marker"
+                className="center-marker"
+            />
+        </div>
+        )
 }
 
 export default Map
