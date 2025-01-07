0101c7e22c7e69c26e80e73ea9ce97fffb1af181
kimDongIll <ehddlf2893@naver.com>
2025-01-07 12:14:29 +0900

    a

--- front/src/page/Login.jsx
+++ front/src/page/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Header from "../components/layout/Header";
+import Header from "../components/contain/Header";
 import InputBox from "../components/InputBox";
 import "../css/login.scss";
 import user from "../../public/icon/Person.svg";
