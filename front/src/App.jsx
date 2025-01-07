1a9f0941c05b1b7a814f48d10e27dd83ba4f39fd
kimDongIll <ehddlf2893@naver.com>
2025-01-07 12:00:23 +0900

    파일 위치 수정

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -12,7 +12,7 @@ import StarScope from "./page/StarScope";
 import VoiceRecord from "./page/VoiceRecord";
 import VoiceRecordList from "./page/VoiceRecordList";
 import DriveFinish from "./page/DriveFinish";
-import Layout from "./components/layout/Layout.jsx";
+import Layout from "./components/Layout.jsx";
 import ProtectedRoute from "./components/layout/ProtectedRoute.jsx";
 import "./css/app.scss";
 import PublicRoute from "./components/layout/PublicRoute.jsx";
